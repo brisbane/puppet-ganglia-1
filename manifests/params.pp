f2c910314a1070ae9e11beb699d84dfa15ed7ead
kashif74 <kashif.alig@gmail.com>
2013-09-05 13:12:55 +0100

    first commit

---
+++ manifests/params.pp
@@ -0,0 +1,16 @@
+class ganglia::params (
+  $ensure = 'installed',
+  $cluster='unspecified',
+  $multicast_address = '239.2.11.71',
+  $owner='unspecified',
+  $send_metadata_interval = 0,
+  $udp_port = '8649',
+  $unicast_listen_port = '8649',
+  $unicast_targets = [],
+  $network_mode ='unicast',
+  $user = 'ganglia',
+  
+)
+{
+  
+}
